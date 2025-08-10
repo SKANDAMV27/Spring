bfe018c99d6e5dffc215e66b067465e7a038907f
unknown <skandagowda0@gmail.com>
2025-08-11 01:27:34 +0530

    in new system

---
+++ demo/src/com/xworkz/demo/external/Bottle.java
@@ -0,0 +1,7 @@
+package com.xworkz.demo.external;
+
+public class Bottle {
+    public void runDemo(){
+        System.out.println("Hello new System Coding Skanda is back...");
+    }
+}
---
+++ demo/src/com/xworkz/demo/runner/MainRunner.java
@@ -0,0 +1,10 @@
+package com.xworkz.demo.runner;
+
+import com.xworkz.demo.external.Bottle;
+
+public class MainRunner {
+    public static void main(String[] args) {
+        Bottle bottle=new Bottle();
+        bottle.runDemo();
+    }
+}
